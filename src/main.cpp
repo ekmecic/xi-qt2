425c35feed1d8af027ab9753d77950819ef3d30f
Emil Ekmecic <e.ekmecic@gmail.com>
2018-06-20 21:16:22 -0400

    [refactor] remove unneeded mainLayout
    Also fixes the double free error, since the layout didn't have the
    main widget set as a parent and therefore its destructor was called
    twice or some shit

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QPlainTextEdit>
 #include <QVBoxLayout>
 
 int main(int argc, char* argv[]) {
@@ -11,23 +10,14 @@ int main(int argc, char* argv[]) {
   Widget w;
   Editor editor(&w);
 
+  // Create a vertical layout and add the editor widget to it, then
+  // remove the margin around the editor widget
+  QVBoxLayout layout(&w);
+  layout.addWidget(&editor);
+  layout.setMargin(0);
+
   // Make sure the window doesn't get too small
   w.setMinimumSize(800, 600);
-
-  // Create a vertical layout and add the editor widget to it
-  QVBoxLayout vLayout;
-  vLayout.addWidget(&editor);
-
-  // Create the container layout and add the vertical layout to it
-  QVBoxLayout mainLayout;
-  mainLayout.addLayout(&vLayout);
-  w.setLayout(&mainLayout);
-
-  // Remove all the margins and spacings
-  vLayout.setSpacing(0);
-  vLayout.setMargin(0);
-  mainLayout.setSpacing(0);
-  mainLayout.setMargin(0);
 
   // Display the window with its child widgets
   w.show();
