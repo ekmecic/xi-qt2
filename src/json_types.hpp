ca576d0b442f75587b929848cd1ea095ffd1319d
Emil Ekmecic <e.ekmecic@gmail.com>
2018-06-23 22:51:45 -0400

    [feature] add map for QString -> XiMethod enum
    We're going to need this when we start handling RPC from Xi, i.e.
    delegating a message to a function in a switch statement.

--- src/json_types.hpp
+++ src/json_types.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QJsonObject>
+#include <QMap>
 #include <QString>
 
 namespace xi_json {
@@ -14,3 +15,33 @@ QJsonObject new_view(QString file_path);
 // Messages sent from xi-core to xi-qt2
 namespace in {}
 } // namespace xi_json
+
+enum class XiMethod {
+  set_style,
+  scroll_to,
+  update,
+  theme_changed,
+  config_changed,
+  available_plugins,
+  plugin_started,
+  plugin_stopped,
+  update_cmds,
+  add_status_item,
+  update_status_item,
+  remove_status_item,
+};
+
+const QMap<QString, XiMethod> xi_method_map = {
+    {"set_style", XiMethod::set_style},
+    {"scroll_to", XiMethod::scroll_to},
+    {"update", XiMethod::update},
+    {"theme", XiMethod::theme_changed},
+    {"config", XiMethod::config_changed},
+    {"available", XiMethod::available_plugins},
+    {"plugin", XiMethod::plugin_started},
+    {"plugin", XiMethod::plugin_stopped},
+    {"update", XiMethod::update_cmds},
+    {"add", XiMethod::add_status_item},
+    {"update", XiMethod::update_status_item},
+    {"remove", XiMethod::remove_status_item},
+};
